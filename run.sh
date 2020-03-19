d31a99559be26c23f49968a42ffcb5ddbcc70f85
Daniel Gómez Martínez <dagomezma@unal.edu.co>
2020-03-19 15:03:39 -0500

    Add files to gitignore file and add .m case datasets from Matpower/IEEE

---
+++ run.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+for ensaio in 1 2 3 4 5
+do
+	for caso in case14.m case30.m case57.m case118.m case300.m case1354pegase.m case2869pegase.m case9241pegase.m;
+	do
+		for exe in P O S;
+		do
+			for num in 1 10 50 100 200
+			do			
+				echo "Starting MAPoL"
+				echo "	Case: $caso"
+				if [ "$exe" = "P" ]; then
+					echo "	Running in: GPU"
+				else
+					echo "	Running in: CPU"
+				fi
+				echo "	# of Particles: $num"
+				echo "	Execution: $ensaio"
+				./Debug/MAPoL datasets/$caso $exe $num
+			done
+		done
+	done
+done
